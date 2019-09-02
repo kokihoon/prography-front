372c7628af8c9fa2c4df481215646c686742bc45
kokihoon <rhrlgns777@gmail.com>
2019-09-02 23:26:17 +0900

    add List using useState, useEffect

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const List = ({todos}) => {
+
+    const todoList = todos.map(todo => <li>{todo}</li>)
+    return (
+        <ul>
+            {todoList}
+        </ul>
+    )
+}
+
+export default List;
